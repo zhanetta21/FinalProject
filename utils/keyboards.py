3c633d106550b12448440a2390b4726ef838b045
lgdgcw <130295479+lgdgcw@users.noreply.github.com>
2026-04-28 10:50:39 +0500

    Update keyboards.py

--- utils/keyboards.py
+++ utils/keyboards.py
@@ -1,8 +1,27 @@
 from telegram import ReplyKeyboardMarkup
 
+
 def main_menu_keyboard():
     keyboard = [
         ["Grades", "Attendance"],
         ["Deadlines", "Schedule"]
+    ]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+
+def grades_keyboard():
+    keyboard = [
+        ["Calculate Total"],
+        ["Needed Score"],
+        ["Back"]
+    ]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+
+def attendance_keyboard():
+    keyboard = [
+        ["Calculate Attendance"],
+        ["Allowed Absences"],
+        ["Back"]
     ]
     return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
