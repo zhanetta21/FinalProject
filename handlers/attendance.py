ae84475c585b190ed091f163b25605595ff302b6
lgdgcw <130295479+lgdgcw@users.noreply.github.com>
2026-04-28 10:53:57 +0500

    Update attendance.py

--- handlers/attendance.py
+++ handlers/attendance.py
@@ -8,3 +8,12 @@ async def attendance_handler(update: Update, context: ContextTypes.DEFAULT_TYPE)
         "- attendance percentage\n"
         "- allowed absences"
     )
+
+from utils.keyboards import attendance_keyboard
+
+
+async def attendance_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Attendance Module\nChoose an option:",
+        reply_markup=attendance_keyboard()
+    )
